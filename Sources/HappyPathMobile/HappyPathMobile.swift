00b9b292507aa17ec23484794a46f701e366f0bf
Erdem İlker Şen <erdem.sen@dk.migrosonline.com>
2024-12-07 13:55:03 +0300

    Initial commit

---
+++ Sources/HappyPathMobile/HappyPathMobile.swift
@@ -0,0 +1,11 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+
+public struct HappyPathMobile {
+    public init() {}
+
+    public func greet(name: String) -> String {
+        return "Hello, \(name)!"
+    }
+}
